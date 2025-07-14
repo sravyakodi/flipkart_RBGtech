531f5c3311773d5c024dd21bb0354c657ce0662e
sravyakodi <sravyakodi2256@gmail.com>
2025-07-14 16:45:04 +0530

    hai everyone

---
+++ flipkart2.java
@@ -0,0 +1,6 @@
+public class flipkart2{
+	public static void main(String[] arg){
+		System.out.println("DONE");
+	}
+}
+
---
+++ flipkart4.java
@@ -0,0 +1,6 @@
+public class flipkart4{
+	public static void main(String[] arg){
+		System.out.println("HAI EVERYONE, SRAVYA HERE");
+	}
+}
+
